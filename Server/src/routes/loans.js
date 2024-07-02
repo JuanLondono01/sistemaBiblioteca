651339f3cf39aa865cc68f0ffb543f0394f13c42
Bama <jelu0204@gmail.com>
2024-07-01 20:52:55 -0500

    http methods for the loan api added

--- Server/src/routes/loans.js
+++ Server/src/routes/loans.js
@@ -1,5 +1,11 @@
-const {Router} = require('express')
-const router = Router()
+const { Router } = require('express');
+const { getAllLoans, loanBook, returnBook } = require('../controllers/loan.controller');
+const router = Router();
 
-router.route('/')
-    .get() //TODO obtener informacion de los prestamos
+router.route('/').get(getAllLoans);
+
+router.route('/').post(loanBook);
+
+router.route('/return/:loanId').post(returnBook);
+
+module.exports = router
