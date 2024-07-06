e067d773a3c356b5160f9d83490e3d4483c10b91
Bama <jelu0204@gmail.com>
2024-07-06 16:23:28 -0500

    added files

--- Client/src/Books/components/Genres.tsx
+++ Client/src/Books/components/Genres.tsx
@@ -5,14 +5,12 @@ import '../styles/genres.css';
 interface GenreProps {
     text: string;
     isActive: boolean;
-    genre: string;
     onClick: () => void;
 }
 
 export const Genres: React.FC<GenreProps> = ({
     text,
     isActive = false,
-    genre,
     onClick,
 }) => {
     return (
@@ -20,8 +18,8 @@ export const Genres: React.FC<GenreProps> = ({
             className={`check-container ${isActive ? 'active-filter' : ''}`}
             onClick={onClick}
         >
-            <input type='checkbox' id={genre} checked={isActive} />
-            <label htmlFor={genre}>
+            <input type='checkbox' />
+            <label>
                 {isActive ? <FaCheck className='checked' /> : null}
                 {text}
             </label>
