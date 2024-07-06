0b6d6c215c44a264b1f4e8ec1d2f6590827acc71
Bama <jelu0204@gmail.com>
2024-07-06 16:25:38 -0500

    SearchBar component added

--- Client/src/Global/Components/SearchBar.tsx
+++ Client/src/Global/Components/SearchBar.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
-
-export const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
+import { useState } from 'react';
+import '../styles/search.css';
+import { IoIosSearch } from 'react-icons/io';
+import { IoIosClose } from 'react-icons/io';
+interface searchProps {
+    search: string;
 }
+
+export const SearchBar: React.FC<searchProps> = ({ search }) => {
+    const [InSearch, setInSearch] = useState(false);
+    const [Search, setSearch] = useState('');
+
+    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setSearch(value);
+        setInSearch(value !== '');
+    };
+
+    const deleteSearchText = () => {
+        setSearch('');
+        setInSearch(false);
+    };
+
+    return (
+        <>
+            <div className='search-container'>
+                <input
+                    type='text'
+                    placeholder={`Search ${search}`}
+                    onChange={onInputChange}
+                    value={Search}
+                    className='search-bar'
+                />
+                {InSearch ? (
+                    <IoIosClose
+                        className='close-icon'
+                        size={20}
+                        onClick={deleteSearchText}
+						color='gray'
+                    />
+                ) : (
+                    <IoIosSearch className='search-icon' size={20} color='gray' />
+                )}
+            </div>
+        </>
+    );
+};
