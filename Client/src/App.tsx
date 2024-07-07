f7de465f12fba67a72ce1353a317c5ec9ad08cde
Bama <jelu0204@gmail.com>
2024-07-06 20:19:05 -0500

    Routes for login and loans added

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,14 +1,18 @@
 import {Route, BrowserRouter, Routes} from 'react-router-dom'
 import { Books } from './Books/Books';
 import { Login } from './Login/Login';
+import { Users } from './Users/Users';
+import { Loans } from './Loans/Loans';
 
 
 export const App = () => {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Books/>}/>
-          <Route path='/login' element={<Login/>}/>
+          <Route path='/Books' element={<Books/>}/>
+          <Route path='/Users' element={<Users/>}/>
+          <Route path='/Login' element={<Login/>}/>
+          <Route path='/Loans' element={<Loans/>}/>
 
         </Routes>
       </BrowserRouter>
