f33af19ea0ecc1e0e639a0238aeab97ade79cee7
Bama <jelu0204@gmail.com>
2024-07-28 19:15:51 -0500

    Button type prop added

--- Client/src/Books/components/Button.tsx
+++ Client/src/Books/components/Button.tsx
@@ -4,15 +4,17 @@ interface ButtonVariant {
     variant: ButtonProps['variant'];
     onClose?: () => void;
     text: string;
+    type?: 'button' | 'submit' | 'reset';
 }
 
 export const BasicButtons: React.FC<ButtonVariant> = ({
     variant,
     text,
     onClose,
+    type
 }) => {
     return (
-        <Button variant={variant} onClick={onClose}>
+        <Button variant={variant} onClick={onClose} type={type}>
             {text}
         </Button>
     );
