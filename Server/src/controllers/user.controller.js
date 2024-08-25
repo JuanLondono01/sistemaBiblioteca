c99aca1571166c4434df2fb5601a1930c4129476
Juan Esteban Londoño Uribe <jelu0204@gmail.com>
2024-08-25 12:52:22 -0500

    double return fixed

--- Server/src/controllers/user.controller.js
+++ Server/src/controllers/user.controller.js
@@ -6,17 +6,12 @@ userCtrl.getUsers = async (req, res) => {
         const users = await User.find().lean();
 
         if (!users) {
-            res.status(404).send('Data not found');
+            return res.status(404).send('Data not found');
         }
 
-        res.json({
-            message: 'Users found',
-            usersData: users,
-        });
-
-        res.status(200).send(users)
+        return res.send(users)
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        return res.status(400).json({ message: error.message });
     }
 };
 
@@ -24,9 +19,9 @@ userCtrl.createUser = async (req, res) => {
     try {
         const newUser = new User(req.body);
         await newUser.save();
-        res.status(201).json(newUser);
+        return res.status(201).json(newUser);
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        return res.status(400).json({ message: error.message });
     }
 };
 
@@ -34,20 +29,19 @@ userCtrl.getUserById = async (req, res) => {
     try {
         const userFound = req.params.id;
         if (!userFound) {
-            res.status(404).json({
+            return res.status(404).json({
                 message: 'User not found',
             });
         }
 
         const userData = await User.findById(userFound);
 
-        res.status(200).json({
+        return res.status(200).json({
             message: 'User found',
             user: userData,
         });
-        res.status(200).send(userData)
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        return res.status(400).json({ message: error.message });
     }
 };
 
@@ -56,15 +50,15 @@ userCtrl.deleteUserByBId = async (req, res) => {
         const deletedUser = await User.findByIdAndDelete(req.params.id);
 
         if (!deletedUser) {
-            res.status(404).send('User not found');
+            return res.status(404).send('User not found');
         }
 
-        res.status(200).json({
+        return res.status(200).json({
             message: 'User deleted',
             userDeleted: deletedUser,
         });
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        return res.status(400).json({ message: error.message });
     }
 };
 
@@ -72,17 +66,19 @@ userCtrl.updateUserById = async (req, res) => {
     try {
         const updatedUser = await User.findByIdAndUpdate(
             req.params.id,
-            req.body
+            req.body,
+            { new: true } // Esto devuelve el usuario actualizado
         );
         if (!updatedUser) {
-            res.status(404).send('User not found');
+            return res.status(404).send('User not found');
         }
-        res.status(200).json({
+        return res.status(200).json({
             message: 'User data updated',
             userUpdated: updatedUser.name,
         });
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        return res.status(400).json({ message: error.message });
     }
 };
+
 module.exports = userCtrl;
